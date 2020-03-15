5a0fb9a7af7f7bdec437b92348f9ab295e6bc685
Sandeep Pasala <sandeep.pasala@gmail.com>
2020-03-15 06:59:51 +0530

    Add files via upload

--- resnet.py
+++ resnet.py
@@ -9,7 +9,7 @@ Reference:
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from eva4models import Net
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -64,9 +64,9 @@ class Bottleneck(nn.Module):
         return out
 
 
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
+class ResNet(Net):
+    def __init__(self, block, num_blocks, num_classes=10, name="Resnet", droupout=0):
+        super(ResNet, self).__init__(name)
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
@@ -75,7 +75,10 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+        #self.gap = nn.AvgPool2d(kernel_size=(4,4)) 
+        self.conv10 = self.create_conv2d(512, 10, kernel_size=(1,1), padding=0, bn=False, relu=False) # IN: 512 OUT:10
+        #self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -92,9 +95,13 @@ class ResNet(nn.Module):
         out = self.layer3(out)
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
+        #out = out.view(out.size(0), -1)
+        #out = self.linear(out)
+        #out = self.gap(out)
+        out = self.conv10(out)
+
+        out = out.view(-1, 10)
+        return F.log_softmax(out, dim=-1)
 
 
 def ResNet18():
